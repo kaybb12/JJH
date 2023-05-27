771bee580e6991500636577c0ded4dcc220bd521
Haesu_Laptop <wogh8615@gmail.com>
2023-05-27 17:10:11 +0900

    액티비티3개팠어요

--- JS/app/src/main/java/com/example/js/MainActivity.java
+++ JS/app/src/main/java/com/example/js/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.js;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_hiragana = (Button) findViewById(R.id.btnHira);
+        Button btn_katakana = (Button) findViewById(R.id.btnKata);
+        Button btn_word = (Button) findViewById(R.id.btnWord);
+
+        btn_hiragana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),hiragana.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_katakana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),katakana.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_word.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(),word.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ JS/app/src/main/java/com/example/js/katakana.java
@@ -0,0 +1,14 @@
+package com.example.js;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class katakana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_katakana);
+    }
+}
