22584cf4a87d16d3b728512761b791c37857bed7
Haesu_Laptop <wogh8615@gmail.com>
2023-05-28 17:46:30 +0900

    히라가나 가타카나 리스트 등록완료 했어요

--- JS/app/src/main/java/com/example/js/words/Hiragana.java
+++ JS/app/src/main/java/com/example/js/words/Hiragana.java
@@ -11,8 +11,90 @@ public class Hiragana {
     public static Vector<Voca> hiragana = new Vector<Voca>();
 
     public Hiragana() {
-        hiragana.add(new Voca("한국어", "일본어"));
-
-        //...
+        //아
+        hiragana.add(new Voca("아", "あ"));
+        hiragana.add(new Voca("이", "い"));
+        hiragana.add(new Voca("우", "う"));
+        hiragana.add(new Voca("에", "え"));
+        hiragana.add(new Voca("오", "お"));
+        //카
+        hiragana.add(new Voca("카", "か"));
+        hiragana.add(new Voca("키", "き"));
+        hiragana.add(new Voca("쿠", "く"));
+        hiragana.add(new Voca("케", "け"));
+        hiragana.add(new Voca("코", "こ"));
+        //사
+        hiragana.add(new Voca("사", "さ"));
+        hiragana.add(new Voca("시", "し"));
+        hiragana.add(new Voca("스", "す"));
+        hiragana.add(new Voca("세", "せ"));
+        hiragana.add(new Voca("소", "そ"));
+        //타
+        hiragana.add(new Voca("타", "た"));
+        hiragana.add(new Voca("치", "ち"));
+        hiragana.add(new Voca("츠", "つ"));
+        hiragana.add(new Voca("테", "て"));
+        hiragana.add(new Voca("토", "と"));
+        //나
+        hiragana.add(new Voca("나", "な"));
+        hiragana.add(new Voca("니", "に"));
+        hiragana.add(new Voca("누", "ぬ"));
+        hiragana.add(new Voca("네", "ね"));
+        hiragana.add(new Voca("노", "の"));
+        //하
+        hiragana.add(new Voca("하", "は"));
+        hiragana.add(new Voca("히", "ひ"));
+        hiragana.add(new Voca("후", "ふ"));
+        hiragana.add(new Voca("헤", "へ"));
+        hiragana.add(new Voca("호", "ほ"));
+        //마
+        hiragana.add(new Voca("마", "ま"));
+        hiragana.add(new Voca("미", "み"));
+        hiragana.add(new Voca("무", "む"));
+        hiragana.add(new Voca("메", "め"));
+        hiragana.add(new Voca("모", "も"));
+        //야
+        hiragana.add(new Voca("야", "や"));
+        hiragana.add(new Voca("유", "ゆ"));
+        hiragana.add(new Voca("요", "よ"));
+        //라
+        hiragana.add(new Voca("라", "ら"));
+        hiragana.add(new Voca("리", "り"));
+        hiragana.add(new Voca("루", "る"));
+        hiragana.add(new Voca("레", "れ"));
+        hiragana.add(new Voca("로", "ろ"));
+        //와
+        hiragana.add(new Voca("와", "わ"));
+        hiragana.add(new Voca("오", "を"));
+        //응
+        hiragana.add(new Voca("응", "ん"));
+        //가
+        hiragana.add(new Voca("가", "が"));
+        hiragana.add(new Voca("기", "ぎ"));
+        hiragana.add(new Voca("구", "ぐ"));
+        hiragana.add(new Voca("게", "げ"));
+        hiragana.add(new Voca("고", "ご"));
+        //자
+        hiragana.add(new Voca("자", "ざ"));
+        hiragana.add(new Voca("지", "じ"));
+        hiragana.add(new Voca("즈", "ず"));
+        hiragana.add(new Voca("제", "ぜ"));
+        hiragana.add(new Voca("조", "ぞ"));
+        //다
+        hiragana.add(new Voca("다", "だ"));
+        hiragana.add(new Voca("데", "で"));
+        hiragana.add(new Voca("도", "ど"));
+        //바
+        hiragana.add(new Voca("바", "ば"));
+        hiragana.add(new Voca("비", "び"));
+        hiragana.add(new Voca("부", "ぶ"));
+        hiragana.add(new Voca("베", "べ"));
+        hiragana.add(new Voca("보", "ぼ"));
+        //파
+        hiragana.add(new Voca("파", "ぱ"));
+        hiragana.add(new Voca("피", "ぴ"));
+        hiragana.add(new Voca("푸", "ぷ"));
+        hiragana.add(new Voca("페", "ぺ"));
+        hiragana.add(new Voca("포", "ぽ"));
     }
 }
--- JS/app/src/main/java/com/example/js/words/Katakana.java
+++ JS/app/src/main/java/com/example/js/words/Katakana.java
@@ -1,4 +1,96 @@
 package com.example.js.words;
 
+import java.util.Vector;
+
 public class Katakana {
+
+    public static Vector<Voca> katakana = new Vector<Voca>();
+
+    public Katakana() {
+        //아
+        katakana.add(new Voca("아", "ア"));
+        katakana.add(new Voca("이", "イ"));
+        katakana.add(new Voca("우", "ウ"));
+        katakana.add(new Voca("에", "エ"));
+        katakana.add(new Voca("오", "オ"));
+        //카
+        katakana.add(new Voca("카", "カ"));
+        katakana.add(new Voca("키", "キ"));
+        katakana.add(new Voca("쿠", "ク"));
+        katakana.add(new Voca("케", "ケ"));
+        katakana.add(new Voca("코", "コ"));
+        //사
+        katakana.add(new Voca("사", "サ"));
+        katakana.add(new Voca("시", "シ"));
+        katakana.add(new Voca("스", "ス"));
+        katakana.add(new Voca("세", "セ"));
+        katakana.add(new Voca("소", "ソ"));
+        //타
+        katakana.add(new Voca("타", "タ"));
+        katakana.add(new Voca("치", "チ"));
+        katakana.add(new Voca("츠", "ツ"));
+        katakana.add(new Voca("테", "テ"));
+        katakana.add(new Voca("토", "ト"));
+        //나
+        katakana.add(new Voca("나", "ナ"));
+        katakana.add(new Voca("니", "ニ"));
+        katakana.add(new Voca("누", "ヌ"));
+        katakana.add(new Voca("네", "ネ"));
+        katakana.add(new Voca("노", "ノ"));
+        //하
+        katakana.add(new Voca("하", "ハ"));
+        katakana.add(new Voca("히", "ヒ"));
+        katakana.add(new Voca("후", "フ"));
+        katakana.add(new Voca("헤", "ヘ"));
+        katakana.add(new Voca("호", "ホ"));
+        //마
+        katakana.add(new Voca("마", "マ"));
+        katakana.add(new Voca("미", "ミ"));
+        katakana.add(new Voca("무", "ム"));
+        katakana.add(new Voca("메", "メ"));
+        katakana.add(new Voca("모", "モ"));
+        //야
+        katakana.add(new Voca("야", "ヤ"));
+        katakana.add(new Voca("유", "ユ"));
+        katakana.add(new Voca("요", "ヨ"));
+        //라
+        katakana.add(new Voca("라", "ラ"));
+        katakana.add(new Voca("리", "リ"));
+        katakana.add(new Voca("루", "ル"));
+        katakana.add(new Voca("레", "レ"));
+        katakana.add(new Voca("로", "ロ"));
+        //와
+        katakana.add(new Voca("와", "ワ"));
+        katakana.add(new Voca("오", "ヲ"));
+        //응
+        katakana.add(new Voca("응", "ン"));
+        //가
+        katakana.add(new Voca("가", "ガ"));
+        katakana.add(new Voca("기", "ギ"));
+        katakana.add(new Voca("구", "グ"));
+        katakana.add(new Voca("게", "ゲ"));
+        katakana.add(new Voca("고", "ゴ"));
+        //자
+        katakana.add(new Voca("자", "ザ"));
+        katakana.add(new Voca("지", "ジ"));
+        katakana.add(new Voca("즈", "ズ"));
+        katakana.add(new Voca("제", "ゼ"));
+        katakana.add(new Voca("조", "ゾ"));
+        //다
+        katakana.add(new Voca("다", "ダ"));
+        katakana.add(new Voca("데", "デ"));
+        katakana.add(new Voca("도", "ド"));
+        //바
+        katakana.add(new Voca("바", "バ"));
+        katakana.add(new Voca("비", "ビ"));
+        katakana.add(new Voca("부", "ブ"));
+        katakana.add(new Voca("베", "ベ"));
+        katakana.add(new Voca("보", "ボ"));
+        //파
+        katakana.add(new Voca("파", "パ"));
+        katakana.add(new Voca("피", "ピ"));
+        katakana.add(new Voca("푸", "プ"));
+        katakana.add(new Voca("페", "ペ"));
+        katakana.add(new Voca("포", "ポ"));
+    }
 }
