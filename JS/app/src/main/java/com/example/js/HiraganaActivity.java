e35baba03f980fcebabf927aab3203644c604bd4
Haesu_Laptop <wogh8615@gmail.com>
2023-05-28 18:02:44 +0900

    히라,가나 리스트 등록 (수정본)

--- JS/app/src/main/java/com/example/js/HiraganaActivity.java
+++ JS/app/src/main/java/com/example/js/HiraganaActivity.java
@@ -3,13 +3,26 @@ package com.example.js;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HiraganaActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiragana);
+
+        Button btnHirahome = (Button) findViewById(R.id.btnHirahome);
+        Button btnHirastart = (Button) findViewById(R.id.btnHirastart);
+
+        btnHirahome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
